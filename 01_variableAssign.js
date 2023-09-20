e8a5ed3ce3f7caaedc90fc5f8c81050b40b17f1f
Bilal Faruk Shaikh <113657831+Bilals37@users.noreply.github.com>
2023-09-20 09:28:24 +0530

    variable_assign
    initialized variables

---
+++ 01_variableAssign.js
@@ -0,0 +1,24 @@
+var myFullName;
+console.log(myFullName);
+var myFullName;
+myFullName= "Bilal Shaikh";
+console.log(myFullName);
+var myFullName;
+myFullName="bilal Faruk Shaikh";
+console.log(myFullName);
+var myLove="I love JS";
+console.log(myLove);
+var firstName="Bilal";
+console.log('First Name:',firstName);
+var lastName='Shaikh'
+console.log('Last Name:', lastName);
+var age='23'
+console.log('Age:', age);
+var address='Shrirampur'
+console.log('Address:', address);
+var pinCode="413709"
+console.log('Pin Code:', pinCode);
+var gender='male'
+console.log('Gender:', gender);
+var isMarried='No'
+console.log('Married:', isMarried);
