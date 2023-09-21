11f64dd1fc2fced2874e762d5a58556f4967421c
Bilal Faruk Shaikh <113657831+Bilals37@users.noreply.github.com>
2023-09-21 12:37:29 +0530

    JS Assignment
    covered Datatypes and variable type

---
+++ 02_typeOf.js
@@ -0,0 +1,24 @@
+console.log('==>(1)');
+var bankName;
+console.log("Before initialization - bankName: " , bankName , ", type: " , typeof bankName);
+bankName = 'SBI Bank';
+console.log("After initialization - bankName: " , bankName , ", type: " , typeof bankName);
+console.log('===========================================================================');
+console.log('==>(2)');
+var marks = '90%';
+console.log("marks: " , marks , ", type: " , typeof marks);
+console.log('===========================================================================');
+console.log('==>(3)');
+var isWorking;
+console.log("Before initialization - isWorking: " , isWorking , ", type: " + typeof isWorking);
+isWorking = true;
+console.log("After initialization - isWorking: " , isWorking , ", type: " , typeof isWorking);
+console.log('===========================================================================');
+console.log('==>(4)');
+var totalCount = "Hundred and Seven";
+totalCount = 107;
+console.log("Initial totalCount: " , totalCount , ", type: " , typeof totalCount, "Updated totalCount: " , totalCount + ", type: " , typeof totalCount );
+
+
+
+
---
+++ 02_variableBasicsassig.js
@@ -0,0 +1,51 @@
+console.log('==================== STRING ====================');
+console.log('');
+var fullName= 'Mohit Sharma';
+console.log('Full Name:', fullName,'||', 'Type of:', typeof fullName);
+var collegeName= 'California State University'
+console.log('College Name:', collegeName, '||','Type of:', typeof collegeName);
+var departmentName= 'Computer Engineering'
+var city= 'California'
+console.log('City', city, '||','Type of:', typeof city);
+console.log('');
+console.log('==================== NUMBER ====================');
+console.log('');
+var sscMarks= 71.25;
+var hscMarks= 65.50;
+var engiMarks= 8.60;
+console.log('Marks:','SSC:' ,sscMarks, '%', '||', 'HSC:', hscMarks,'%', '||', 'Engineering:', engiMarks,'CGPA','||','Type of:', typeof engiMarks );
+var pincode= 854796;
+console.log('Pincode:',pincode, '||', 'Type of:', typeof pincode);
+var mobileNum= 4716854715;
+console.log('Mobile Number:', mobileNum, '||','Type of:', typeof city);
+console.log('');
+console.log('==================== BOOLEAN ===================');
+console.log('');
+var isdisabled= false;
+console.log('isabled');
+console.log('Disabled:', isdisabled ,'||', 'Type of:', typeof isdisabled);
+var isMarried= false;
+console.log('Married:', isMarried,'||','Type of:', typeof isMarried );
+var Indian= true;
+console.log('Indian', Indian, '||', 'Type of:', typeof Indian);
+console.log('');
+console.log('===== DIFFERENCE BETWEEN UNDEFIEND & NULL =====');
+console.log('');
+console.log('================== UNDEFIEND ==================');
+console.log('');
+console.log('Undefiend ==> Undefiend means a variable has been declared bur the value is nat assigned to variable this types of outputa are callwd as undefiend.');
+console.log('for Example ==>');
+console.log('var salary;');
+console.log('console.log(salary);');
+console.log('Output ==> undefiend');
+console.log('');
+console.log('==================== NULL =====================');
+console.log('');
+console.log('NULL ==> Null value is assigned to a variable when we wanna keep value as empty or if we dont know the variable value ');
+console.log('for Example ==>');
+console.log('var salary= null;');
+console.log('console.log(salary);');
+console.log('Output ==> null');
+
+
+
